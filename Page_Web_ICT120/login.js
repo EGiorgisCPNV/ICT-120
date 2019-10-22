401f1c1fb8dee6a65d88c4fb28f43be7ca747889
EGiorgisCPNV <48352389+EGiorgisCPNV@users.noreply.github.com>
2019-10-22 16:57:48 +0200

    ajout de glyphicon dans la page login

--- Page_Web_ICT120/login.js
+++ Page_Web_ICT120/login.js
@@ -1,20 +1,21 @@
+
 //fonction qui permet d'afficher le mot de passe
 function SwitchPass() {
     var typeInput = document.getElementById('mdp');
-    var txtHREF = document.getElementById('AffPass');
+    var txtHREF = document.getElementById('oeil');
 
     if (typeInput.type == 'password') {
         typeInput.type = 'text';
-        txtHREF.innerHTML = 'Cacher le mot de passe';
+        document.getElementById("oeil").className = "glyphicon glyphicon-eye-close";
     } else {
         typeInput.type = 'password';
-        txtHREF.innerHTML = 'Afficher mot de passe';
+        document.getElementById("oeil").className = "glyphicon glyphicon-eye-open";
     }
 
 }
 
 
-//fonction pour enrgistrer l'email et le mdp dans un fichier experne [(mais elle ne marche pas)]
+//fonction pour enrgistrer l'email et le mdp dans un fichier externe (mais elle ne marche pas)
 function Stockage(){
     a=document.getElementById("mdp");
     let rootDirEntry = fileEntry.filesystem.root;
