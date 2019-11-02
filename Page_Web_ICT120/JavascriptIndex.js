dd2319c429ac8f8b3e7dbf458d479e00dac725e0
EGiorgisCPNV <48352389+EGiorgisCPNV@users.noreply.github.com>
2019-11-02 13:33:03 +0100

    amelioration général

--- Page_Web_ICT120/JavascriptIndex.js
+++ Page_Web_ICT120/JavascriptIndex.js
@@ -1,4 +1,3 @@
-
 //fonction qui permet d'afficher le mot de passe
 function SwitchPass() {
     var typeInput = document.getElementById('mdp');
@@ -16,11 +15,11 @@ function SwitchPass() {
 
 
 //fonction pour enrgistrer l'email et le mdp dans un fichier externe (mais elle ne marche pas)
-function Stockage(){
+function Stockage() {
 
-    var emailAStocker=document.getElementById("email").value;
+    var emailAStocker = document.getElementById("email").value;
 
-    var mdpAStocker=document.getElementById("mdp").value;
+    var mdpAStocker = document.getElementById("mdp").value;
 
     // Sauvegarder les informations dans l’espace local courant
     localStorage.setItem("login", emailAStocker);
@@ -36,29 +35,27 @@ function Stockage(){
 }
 
 
-
 //fonction qui verifie si l'email et le mdp si il correspond au compte register
-function Verification(){
+function Verification() {
 
     var verificationLogin = localStorage.getItem("login");
     var verificationMdp = localStorage.getItem("motDePasse");
 
-    var recupEmail= document.getElementById("email").value;
+    var recupEmail = document.getElementById("email").value;
     var recupMdp = document.getElementById("mdp").value;
 
-   if(verificationLogin == recupEmail && verificationMdp == recupMdp){
-       window.location.replace("page_activité_avec_login.html");
-   }
-   else {
+    if (verificationLogin == recupEmail && verificationMdp == recupMdp) {
+        window.location.replace("page_activité_avec_login.html");
+    } else {
 
-       alert('Error message: ' + "L'email ou le mot de passe est incorrect");
-       window.location.replace("login.html");
-   }
+        alert('Error message: ' + "L'email ou le mot de passe est incorrect");
+        window.location.replace("login.html");
+    }
 
 }
 
 //fonction pour pouvoir chercher dans la barre de recherche des éléments du tableau (NE FONCTIONNE PAS)
-function myFunction1() {
+function Recherche() {
     var input, filter, table, tr, td, i, txtValue;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
@@ -82,66 +79,70 @@ function myFunction11() {
     var table = document.getElementById("table");
     var row = table.insertRow(0);
     var cell1 = row.insertCell(0);
-    var a =document.createElement("td");
+    var a = document.createElement("td");
 
     list1.appendChild(a);
 
 }
 
-//fonction qui ajouter une ligne au tr
-function myFunction() {
-    var row = document.getElementById("lignePourUneActivitee"); // find row to copy
-    var table = document.getElementById("table"); // find table to append to
-    var clone = row.cloneNode(true); // copy children too
 
-    clone.id = "newID"; // change id or other attributes/contents
-    table.appendChild(clone); // add new row to end of table
-
-
-}
-
-
-
-//fonction qui supprime une ligne
-function Delete(){
-    document.getElementById('lignePourUneActivitee').deleteRow(-1);
-
-}
-
-//fonction qui genere un tableau
-function CreatTableau1231233123(){
-    //prend la valeur du premier input
-    var a=document.getElementById("premierInput").value;
-
-    //cree un new input
-    var field = document.createElement("input");
-    field.type=text;
-    field.id="WESH";
-    field.name = field.id;
-
-    document.createElement('<input type="text" name="'+lenom+'" >')
-
-}
-
-
-//fonction qui genere un tableau
-function CreatTableau(){
+//fonction qui crée une a chaque fois que l'on clique sur ajouter
+function CreatTableau() {
     var table = document.getElementById("myTable");
+
     var row = table.insertRow(0);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    var cell6 = row.insertCell(5);
+    var cell7 = row.insertCell(6);
+
 
     var valeurIndex0 =document.getElementById("index0").value;
     var valeurIndex1 =document.getElementById("index1").value;
     var valeurIndex2 =document.getElementById("index2").value;
     var valeurIndex3 =document.getElementById("index3").value;
+    var valeurIndex4 =document.getElementById("index4").value;
+    var valeurIndex5 =document.getElementById("index5").value;
+    var valeurIndex6 =document.getElementById("index6").value;
+
 
     cell1.innerHTML = valeurIndex0;
     cell2.innerHTML = valeurIndex1;
     cell3.innerHTML = valeurIndex2;
     cell4.innerHTML = valeurIndex3;
+    cell5.innerHTML = valeurIndex4;
+    cell6.innerHTML = valeurIndex5;
+    cell7.innerHTML = valeurIndex6;
+
 
 }
 
+
+
+//fonction qui supprimme une ligne du dessus
+function Delete() {
+    for(var i=0;i<1;i++){
+        document.getElementById('myTable').deleteRow(i);
+
+    }
+}
+
+
+//fonction qui redirige sur la page d'accueil sans etre login
+function Deconnection(){
+    window.location.replace("page_activite_sans_login.html");
+}
+
+//fonction qui redirige sur la page d'accueil en étant login login
+function Login(){
+    window.location.replace("login.html");
+}
+
+
+//fonction qui redirige sur la page d'accueil en étant login login
+function Register(){
+    window.location.replace("register.html");
+}
